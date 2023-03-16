8e743b0b22852d85daaf047059a293d0e463b0f6
Anton Putra <me@antonputra.com>
2023-03-16 12:04:30 -0700

    asd

--- lessons/158/terraform/4-fluentd.tf
+++ lessons/158/terraform/4-fluentd.tf
@@ -1,9 +1,24 @@
-resource "helm_release" "kibana" {
+resource "helm_release" "fluentd" {
   name = "logging"
 
   repository       = "https://charts.bitnami.com/bitnami"
   chart            = "fluentd"
-  namespace        = "elk"
+  namespace        = "logging"
   create_namespace = true
-  version          = "0.3.9"
+  version          = "5.5.16"
+
+  set {
+    name  = "forwarder.tolerations[0].key"
+    value = "fluentd"
+  }
+
+  set {
+    name  = "forwarder.tolerations[0].operator"
+    value = "Exists"
+  }
+
+  set {
+    name  = "forwarder.tolerations[0].effect"
+    value = "NoSchedule"
+  }
 }
