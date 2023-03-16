f9264156906e19fb17df858022f1f71f09c83919
Anton Putra <me@antonputra.com>
2023-03-16 13:18:56 -0700

    asd

--- lessons/158/terraform/4-fluentd.tf
+++ lessons/158/terraform/4-fluentd.tf
@@ -7,27 +7,5 @@ resource "helm_release" "fluentd" {
   create_namespace = true
   version          = "5.5.16"
 
-  values = [
-    "${file("values/fluentd.yaml")}"
-  ]
-
-  set {
-    name  = "forwarder.tolerations[0].key"
-    value = "fluentd"
-  }
-
-  set {
-    name  = "forwarder.tolerations[0].operator"
-    value = "Exists"
-  }
-
-  set {
-    name  = "forwarder.tolerations[0].effect"
-    value = "NoSchedule"
-  }
-
-  set {
-    name  = "aggregator.enabled"
-    value = "false"
-  }
+  values = [file("values/fluentd.yaml")]
 }
