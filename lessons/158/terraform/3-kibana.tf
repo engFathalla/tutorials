98641d7b48088a76d4d9be22a86b3d418f1b74da
Anton Putra <me@antonputra.com>
2023-03-16 11:40:44 -0700

    asd

--- lessons/158/terraform/3-kibana.tf
+++ lessons/158/terraform/3-kibana.tf
@@ -18,13 +18,18 @@ resource "helm_release" "kibana" {
   }
 
   set {
+    name  = "service.type"
+    value = "LoadBalancer"
+  }
+
+  set {
     name  = "elasticsearch.hosts[0]"
     value = "elk-elasticsearch-coordinating-hl.elk"
   }
 
   set {
     name  = "elasticsearch.port"
-    value = "port"
+    value = "9200"
   }
 
   set {
