adddb2d45916284fffceafc6805e90ce5fa8b6c4
Anton Putra <me@antonputra.com>
2023-03-15 21:01:53 -0700

    asd

--- lessons/158/README.md
+++ lessons/158/README.md
@@ -15,3 +15,15 @@ sudo mount -t ext4 /dev/sdb /mnt/disks/$DISK_UUID
 
 echo UUID=`sudo blkid -s UUID -o value /dev/sdb` /mnt/disks/$DISK_UUID ext4 defaults 0 2 | sudo tee -a /etc/fstab
 
+
+
+
+sudo mkfs.ext4 /dev/sdc
+DISK_UUID=$(sudo blkid -s UUID -o value /dev/sdc) 
+sudo mkdir /mnt/disks/$DISK_UUID
+sudo mount -t ext4 /dev/sdc /mnt/disks/$DISK_UUID
+
+echo UUID=`sudo blkid -s UUID -o value /dev/sdc` /mnt/disks/$DISK_UUID ext4 defaults 0 2 | sudo tee -a /etc/fstab
+
+
+kubectl label node node-00 service=elk
--- lessons/158/terraform/2-elasticsearch.tf
+++ lessons/158/terraform/2-elasticsearch.tf
@@ -1,19 +1,44 @@
-# resource "helm_release" "elk" {
-#   name = "elk"
+resource "helm_release" "elk" {
+  name = "elk"
 
-#   repository       = "https://charts.bitnami.com/bitnami"
-#   chart            = "elasticsearch"
-#   namespace        = "elk"
-#   create_namespace = true
-#   version          = "19.5.14"
+  repository       = "https://charts.bitnami.com/bitnami"
+  chart            = "elasticsearch"
+  namespace        = "elk"
+  create_namespace = true
+  version          = "19.5.14"
 
-#   set {
-#     name  = "data.persistence.storageClass"
-#     value = "ssd"
-#   }
+  set {
+    name  = "data.persistence.storageClass"
+    value = "ssd"
+  }
 
-#   set {
-#     name  = "data.persistence.size"
-#     value = "100Gi"
-#   }
-# }
+  set {
+    name  = "data.persistence.size"
+    value = "97Gi"
+  }
+
+  set {
+    name  = "master.masterOnly"
+    value = "false"
+  }
+
+  set {
+    name  = "master.persistence.storageClass"
+    value = "ssd"
+  }
+
+  set {
+    name  = "master.nodeSelector.service"
+    value = "elk"
+  }
+
+  set {
+    name  = "data.nodeSelector.service"
+    value = "elk"
+  }
+
+  set {
+    name  = "coordinating.nodeSelector.service"
+    value = "elk"
+  }
+}
